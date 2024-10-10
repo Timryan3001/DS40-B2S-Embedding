d81880e80ea887559f5dc836828cdc9bb4a44d69
Timryan3001 <timryan3001@gmail.com>
2024-10-10 14:32:41 +0100

    adding functions

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ let workbook;
 let vizActiveSheet;
 let dashboard;
 let listSheets;
+let saleMap;
+let totalSales;
+let salesByProduct;
+let salesBySegment;
 
 // Function to log workbok information
 
@@ -31,6 +35,40 @@ function logWorkbookInformation() {
         worksheetName = element.name;
         console.log(`The worksheet with index ${index} is called ${worksheetName}`);
     });
+
+    saleMap = listSheets.find((ws) => ws.name == "SaleMap");
+    totalSales = listSheets.find((ws) => ws.name == "Total Sales");
+    salesByProduct = listSheets.find((ws) => ws.name == "SalesbyProduct");
+    salesBySegment = listSheets.find((ws) => ws.name == "SalesbySegment");
 }
 
-viz.addEventListener("firstinteractive", logWorkbookInformation)
+function oregonWashFunction() {
+    console.log(oregonWashingtonButton.value);
+    saleMap.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+    totalSales.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+    salesByProduct.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+    salesBySegment.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+}
+
+function clearStateFilter() {
+    console.log(clearFilterButton.value);
+    saleMap.clearFilterAsync("State");
+    totalSales.clearFilterAsync("State");
+    salesByProduct.clearFilterAsync("State");
+    salesBySegment.clearFilterAsync("State");
+}
+
+function unDo() {
+    console.log(undoButton.value);
+    viz.undoAsync();
+}
+
+const oregonWashingtonButton = document.getElementById("oregon_and_washington");
+const clearFilterButton = document.getElementById("clear_filter");
+const undoButton = document.getElementById("undo");
+
+viz.addEventListener("firstinteractive", logWorkbookInformation);
+
+oregonWashingtonButton.addEventListener("click", oregonWashFunction);
+clearFilterButton.addEventListener("click", clearStateFilter);
+undoButton.addEventListener("click", unDo);
